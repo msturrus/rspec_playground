1a70e93bdf1d81a9663195531b7923e6af3cd4f6
Michael Sturrus <msturrus@gmail.com>
2016-05-02 09:46:41 -0500

    test is example, test is not failure.

--- test/movie_test.rb
+++ test/movie_test.rb
@@ -8,4 +8,14 @@ describe Movie do
   it "should return a hash" do
     Movie.new.to_hash.class.should == Hash
   end
+
+  it "should be Solarbabies" do
+    @sb = Movie.new.to_hash
+    @sb['Title'].should == 'Solarbabies'
+  end
+
+  it "should be Solarbabies" do
+    @sb = Movie.new.to_hash
+    @sb['Year'].should == '1986'
+  end
 end
